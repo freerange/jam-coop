9f9f194183a925cd1fb3b9a097482836afed0c0d
Chris Lowis <chris.lowis@gmail.com>
2023-08-03 15:44:10 -0500

    Switch to cuprite for system tests
    It's faster and seems to work on my machine with Chrome. Before this I
    was running into this issue[1] where it seems that webdrivers wasn't
    up-to-date with the latest available Chrome. Switching to cuprite and
    removing webdrivers from the gem file seems to fix things.
    
    [1] https://github.com/titusfortner/webdrivers/issues/247

--- Gemfile
+++ Gemfile
@@ -75,6 +75,6 @@ end
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem 'capybara'
+  gem 'cuprite'
   gem 'selenium-webdriver'
-  gem 'webdrivers'
 end
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'capybara/cuprite'
+
+Capybara.javascript_driver = :cuprite
+Capybara.register_driver(:cuprite) do |app|
+  Capybara::Cuprite::Driver.new(app, window_size: [1400, 1400])
+end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_firefox, screen_size: [1400, 1400]
+  driven_by :cuprite
 end
