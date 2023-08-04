47b43c68bf6f2e9e3b05c27937136e480eef480f
Chris Lowis <chris.lowis@gmail.com>
2023-08-04 11:40:46 -0500

    Remove "respond_to" from InterestsController
    We're only responding to HTML at the moment, so we don't need this
    additional block.

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,22 +1,18 @@
 # frozen_string_literal: true
 
 class InterestsController < ApplicationController
-  # GET /interests/new
   def new
     @interest = Interest.new
   end
 
-  # POST /interests or /interests.json
   def create
     @interest = Interest.find_or_create_by(interest_params)
 
-    respond_to do |format|
-      if @interest.save
-        InterestsMailer.confirm(@interest).deliver_now
-        format.html { redirect_to thankyou_url }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @interest.save
+      InterestsMailer.confirm(@interest).deliver_now
+      redirect_to thankyou_url
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
