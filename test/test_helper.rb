801323271ea7fa4db17b22cbfad5c95eedf56bf6
Chris Lowis <chris.lowis@gmail.com>
2023-08-16 20:43:14 -0500

    Remove fixture configuration
    We'll use FactoryBot instead of fixtures.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,10 +10,5 @@ module ActiveSupport
 
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-
-    # Add more helper methods to be used by all tests here...
   end
 end
