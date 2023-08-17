781bc943e5677be4b728ccaa70f6614dbd52da59
Chris Lowis <chris.lowis@gmail.com>
2023-08-17 11:11:15 -0500

    Use friendly id based on name for Artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Artist < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_030335) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_17_160814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_030335) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_artists_on_slug", unique: true
   end
 
   create_table "interests", force: :cascade do |t|
