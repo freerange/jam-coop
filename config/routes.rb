3da02e2e15689169e59d9c3ac8e08d6a4bc6735a
Chris Lowis <chris.lowis@gmail.com>
2023-08-18 13:41:54 -0500

    Remove sign_up / registration feature
    For now we don't want to allow anyone to sign up, we're going to
    restrict signing in to users that we have created manually.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@
 Rails.application.routes.draw do
   get  'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
-  get  'sign_up', to: 'registrations#new'
-  post 'sign_up', to: 'registrations#create'
 
   get  'home', to: 'home#index'
 
