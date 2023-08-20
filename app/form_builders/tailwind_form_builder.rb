50056b7646d6f22fe42667d266d76594db926bcc
Chris Lowis <chris.lowis@gmail.com>
2023-08-20 08:36:07 -0500

    Use a form builder to apply tailwind styles to sign in form
    The tailwind styles for form input elements can become quite
    repetitive. Neither extracting partials not using a component
    library (like ViewComponent) field like the right thing to do for
    now. Instead, I've cribbed this pattern from the testdouble blog[1],
    and separated the layout styles from the presentational ones using a
    form builder.
    
    I haven't spent too much time looking at all the possible form
    elements and error styles yet, there may be some things I want to
    tweak in this builder later.
    
    [1] https://blog.testdouble.com/posts/2022-12-05-blending-tailwind-with-rails-forms/

---
+++ app/form_builders/tailwind_form_builder.rb
@@ -0,0 +1,109 @@
+# frozen_string_literal: true
+
+class TailwindFormBuilder < ActionView::Helpers::FormBuilder
+  class_attribute :text_field_helpers,
+                  default: field_helpers - %i[label check_box radio_button fields_for fields hidden_field file_field]
+  #  leans on the FormBuilder class_attribute `field_helpers`
+  #  you'll want to add a method for each of the specific helpers listed here if you want to style them
+
+  TEXT_FIELD_STYLE = 'border border-slate-200 outline-none px-3 py-2'
+  SELECT_FIELD_STYLE = 'block'
+  SUBMIT_BUTTON_STYLE = 'py-3 px-5 bg-amber-600 hover:bg-amber-500 text-white font-medium cursor-pointer'
+
+  text_field_helpers.each do |field_method|
+    class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1 # rubocop:disable Style/DocumentDynamicEvalDefinition
+          def #{field_method}(method, options = {})
+            if options.delete(:tailwindified)
+              super
+            else
+              text_like_field(#{field_method.inspect}, method, options)
+            end
+          end
+    RUBY_EVAL
+  end
+
+  def submit(value = nil, options = {})
+    custom_opts, opts = partition_custom_opts(options)
+    classes = apply_style_classes(SUBMIT_BUTTON_STYLE, custom_opts)
+
+    super(value, { class: classes }.merge(opts))
+  end
+
+  def select(method, choices = nil, options = {}, html_options = {}, &)
+    custom_opts, opts = partition_custom_opts(options)
+    classes = apply_style_classes(SELECT_FIELD_STYLE, custom_opts, method)
+
+    labels = labels(method, custom_opts[:label], options)
+    field = super(method, choices, opts, html_options.merge({ class: classes }), &)
+
+    labels + field
+  end
+
+  private
+
+  def text_like_field(field_method, object_method, options = {})
+    custom_opts, opts = partition_custom_opts(options)
+
+    classes = apply_style_classes(TEXT_FIELD_STYLE, custom_opts, object_method)
+
+    field = send(field_method, object_method, {
+      class: classes,
+      title: errors_for(object_method)&.join(' ')
+    }.compact.merge(opts).merge({ tailwindified: true }))
+
+    labels = labels(object_method, custom_opts[:label], options)
+
+    labels + field
+  end
+
+  def labels(object_method, label_options, field_options)
+    label = tailwind_label(object_method, label_options, field_options)
+    error_label = error_label(object_method, field_options)
+
+    @template.content_tag('div', label + error_label, { class: 'flex flex-col items-start' })
+  end
+
+  def tailwind_label(object_method, label_options, field_options)
+    text, label_opts = if label_options.present?
+                         [label_options[:text], label_options.except(:text)]
+                       else
+                         [nil, {}]
+                       end
+
+    label_classes = label_opts[:class] || 'block text-slate-600 mb-1 md:mb-0 pr-4'
+    label_classes += ' text-yellow-800 dark:text-yellow-400' if field_options[:disabled]
+    label(object_method, text, {
+      class: label_classes
+    }.merge(label_opts.except(:class)))
+  end
+
+  def error_label(object_method, options)
+    return if errors_for(object_method).blank?
+
+    error_message = @object.errors[object_method].collect(&:titleize).join(', ')
+    tailwind_label(object_method, { text: error_message, class: ' font-bold text-red-500' }, options)
+  end
+
+  def border_color_classes(object_method)
+    if errors_for(object_method).present?
+      ' border-2 border-red-400 focus:border-rose-200'
+    else
+      ' border border-gray-300 focus:border-yellow-700'
+    end
+  end
+
+  def apply_style_classes(classes, custom_opts, object_method = nil)
+    classes + border_color_classes(object_method) + " #{custom_opts[:class]}"
+  end
+
+  CUSTOM_OPTS = %i[label class].freeze
+  def partition_custom_opts(opts)
+    opts.partition { |k, _v| CUSTOM_OPTS.include?(k) }.map(&:to_h)
+  end
+
+  def errors_for(object_method)
+    return unless @object.present? && object_method.present?
+
+    @object.errors[object_method]
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module MusicCoop
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join('app/form_builders')
   end
 end
