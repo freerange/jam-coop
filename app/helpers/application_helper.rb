317faae48ec66ad98456da1fa4d6398dde6837c4
Chris Lowis <chris.lowis@gmail.com>
2023-08-20 21:01:00 -0500

    Apply default styles to link_to

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include ActionView::Helpers::UrlHelper
+  alias original_link_to link_to
+
+  def link_to(*, **kwargs)
+    kwargs[:class] = "#{kwargs[:class] || ''} underline decoration-amber-600 hover:bg-amber-500"
+    original_link_to(*, **kwargs)
+  end
 end
