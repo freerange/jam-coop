c348c93ee228f4d86e7f1b9ab479213f921ff252
Chris Lowis <chris.lowis@gmail.com>
2023-08-21 16:56:33 -0500

    Rename ApplicationHelper#{link_to, text_link_to}
    I didn't think it through when I added this helper method - it's very
    useful to be able to call the original link_to helper in other places.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,9 @@
 
 module ApplicationHelper
   include ActionView::Helpers::UrlHelper
-  alias original_link_to link_to
 
-  def link_to(*, **kwargs)
+  def text_link_to(*, **kwargs)
     kwargs[:class] = "#{kwargs[:class] || ''} underline decoration-amber-600 hover:bg-amber-500"
-    original_link_to(*, **kwargs)
+    link_to(*, **kwargs)
   end
 end
