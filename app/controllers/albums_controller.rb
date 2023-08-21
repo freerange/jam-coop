dee77b0cd1b558306f7e23b6fb766a56040eb74e
Chris Lowis <chris.lowis@gmail.com>
2023-08-21 16:04:53 -0500

    Basic interface for adding new albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,9 +2,45 @@
 
 class AlbumsController < ApplicationController
   skip_before_action :authenticate
+  before_action :set_album, only: %i[show edit update]
 
-  def show
-    artist = Artist.friendly.find(params[:artist_id])
+  def show; end
+
+  def new
+    @album = Album.new(artist:)
+  end
+
+  def edit; end
+
+  def create
+    @album = artist.albums.new(album_params)
+
+    if @album.save
+      redirect_to artist_url(artist), notice: 'Artist was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @lbum.update(album_params)
+      redirect_to artist_url(@artist), notice: 'Artist was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_album
     @album = artist.albums.friendly.find(params[:id])
+  end
+
+  def artist
+    Artist.friendly.find(params[:artist_id])
+  end
+
+  def album_params
+    params.require(:album).permit(:title, :cover)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   end
 
   resources :artists do
-    resources :albums, only: %i[show]
+    resources :albums, only: %i[show new edit create update]
   end
 
   get 'thankyou', to: 'interests#thankyou'
