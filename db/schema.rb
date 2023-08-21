a08685388e5ae867ac1b0988a94f5116bc649296
Chris Lowis <chris.lowis@gmail.com>
2023-08-20 21:28:23 -0500

    Remove accidental changes to db/schema.rb
    I accidentally added these changes in 959d66a based on the state of my
    local db from another branch.

--- db/schema.rb
+++ db/schema.rb
@@ -14,52 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_163316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "albums", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug"
-    t.bigint "artist_id", default: 1, null: false
-    t.index ["artist_id"], name: "index_albums_on_artist_id"
-    t.index ["slug", "artist_id"], name: "index_albums_on_slug_and_artist_id", unique: true
-  end
-
-  create_table "artists", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug"
-    t.index ["slug"], name: "index_artists_on_slug", unique: true
-  end
-
   create_table "email_verification_tokens", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_email_verification_tokens_on_user_id"
@@ -88,15 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_163316) do
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
-  create_table "tracks", force: :cascade do |t|
-    t.string "title"
-    t.integer "position"
-    t.bigint "album_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["album_id"], name: "index_tracks_on_album_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
@@ -106,9 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_163316) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "albums", "artists"
   add_foreign_key "email_verification_tokens", "users"
   add_foreign_key "password_reset_tokens", "users"
   add_foreign_key "sessions", "users"
