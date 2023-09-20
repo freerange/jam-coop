3bfd5ec1d2a1f15f5ab1a15611b20c7e3b4b4281
Chris Lowis <chris.lowis@gmail.com>
2023-09-20 09:39:37 +0100

    Upload tracks directly to S3
    In production (on render) I noticed that when I upload large files to
    the tracks model the rails server can time out. This makes sense since
    a WAV file runs at around 10MB/min. To improve the experience I've
    implemented direct-to-S3 uploading as per the ActiveStorage rails
    guide[1]. This commit just implements the file upload part, there's no
    additional JS to provide a progress bar or similar yet.
    
    I've also added a CORS configuration to the development and production
    buckets as per the guide[2].
    
    [1] https://edgeguides.rubyonrails.org/active_storage_overview.html#direct-uploads
    [2]
    https://edgeguides.rubyonrails.org/active_storage_overview.html#example-s3-cors-configuration

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import * as ActiveStorage from "@rails/activestorage"
+ActiveStorage.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,5 @@ pin 'application', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin '@rails/activestorage', to: 'activestorage.esm.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
