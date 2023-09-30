9cb8a571ba431badc785d17288d8c3956e81180a
Chris Lowis <chris.lowis@gmail.com>
2023-09-30 07:37:22 +0100

    Play/pause the audio when the button is pressed

--- app/javascript/controllers/player_controller.js
+++ app/javascript/controllers/player_controller.js
@@ -2,15 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="player"
 export default class extends Controller {
-  static targets = ["playButton", "pauseButton"]
+  static targets = ["playButton", "pauseButton", "audio"]
 
   play() {
     this.playButtonTarget.classList.add("hidden");
     this.pauseButtonTarget.classList.remove("hidden");
+
+    this.audioTarget.play();
   }
 
   pause() {
     this.pauseButtonTarget.classList.add("hidden");
     this.playButtonTarget.classList.remove("hidden");
+
+    this.audioTarget.pause();
   }
 }
