7fcec0b703802f5f525dff9b92eee4512ee31978
Chris Lowis <chris.lowis@gmail.com>
2023-09-30 21:15:13 +0100

    Play the next track when the current one finishes

--- app/javascript/controllers/player_controller.js
+++ app/javascript/controllers/player_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="player"
 export default class extends Controller {
-  static targets = ["playButton", "pauseButton", "audio"]
+  static targets = ["playButton", "pauseButton", "audio", "track"]
 
   play() {
     this.playButtonTarget.classList.add("hidden");
@@ -19,6 +19,33 @@ export default class extends Controller {
   }
 
   ended() {
-    this.pause();
+    if (this.nextTrack()) {
+      this.audioTarget.src = this.nextTrack();
+      this.audioTarget.play();
+    } else {
+      this.pause();
+      this.audioTarget.src = this.firstTrack();
+    }
+  }
+
+  trackList() {
+    return this.trackTargets.map(function(t) { return t.dataset.trackUrl});
+  }
+
+  firstTrack() {
+    return this.trackList()[0]
+  }
+
+  currentTrack() {
+    const url = new URL(this.audioTarget.src);
+
+    return url.pathname
+  }
+
+  nextTrack() {
+    const currentIndex = this.trackList().indexOf(this.currentTrack());
+    const nextIndex = currentIndex + 1;
+
+    return this.trackList()[nextIndex]
   }
 }
