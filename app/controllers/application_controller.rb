4d010e3cf395844d4e9b0e2e8edcaedd3aa05252
Chris Lowis <chris.lowis@gmail.com>
2023-10-02 21:12:28 +0100

    Ensure Current.session is set on every request
    Before this change we only set Current.session (that is, whether a
    user is logged in and has a current session set) on calls to
    `authenticate`. In order to allow us to differentiate between
    signed-in and signed-out users in our controller actions and views we
    need to perform this check on every request.
    
    With this change the #authenticate method can just check for the
    presence of a Current.user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,12 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate
-    if (session_record = Session.find_by(id: cookies.signed[:session_token]))
-      Current.session = session_record
-    else
-      redirect_to sign_in_path
-    end
+    redirect_to sign_in_path unless Current.user
   end
 
   def set_current_request_details
     Current.user_agent = request.user_agent
     Current.ip_address = request.ip
+    Current.session = Session.find_by(id: cookies.signed[:session_token])
   end
 end
