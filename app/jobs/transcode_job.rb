155f23b5375c54975ef5ddb4fc442a32f60b9d8f
Chris Lowis <chris.lowis@gmail.com>
2023-10-13 20:54:41 +0100

    Allow TranscodeJob to transcode to CBR MP3 (128kb/s)
    The ffmpeg parameters for this file type are mostly the same as in
    8f4110ee but with a constant bit rate of 128kb/s.
    
    I think this file format will be useful as the default
    streaming/previewing one as it results in a lower file size (meaning
    faster delivery over slower connections). Since we don't have any
    mechanism of stopping visitors downloading the files we use for
    previewing it also provides an incentive for them to buy, since a
    128kb/s MP3 will sound OK, but have some artifacts not present in our
    better quality and larger transcodes.

--- app/jobs/transcode_job.rb
+++ app/jobs/transcode_job.rb
@@ -7,7 +7,7 @@ class TranscodeJob < ApplicationJob
     output_fn = "#{track.original.filename.base}.mp3"
 
     Tempfile.create('transcode') do |output|
-      track.original.open { |file| transcode(file, output) }
+      track.original.open { |file| transcode(file, output, format) }
       track.transcodes.where(format:).destroy_all
       transcode = track.transcodes.create(format:)
       transcode.file.attach(io: File.open(output.path), filename: output_fn, content_type: 'audio/mpeg')
@@ -16,8 +16,16 @@ class TranscodeJob < ApplicationJob
 
   private
 
-  def transcode(input, output)
-    cmd = "ffmpeg -y -nostats -loglevel 0 -i #{input.path} -codec:a libmp3lame -q:a 0 -f mp3 #{output.path}"
+  def transcode(input, output, format)
+    case format
+    when :mp3v0
+      cmd = "ffmpeg -y -nostats -loglevel 0 -i #{input.path} -codec:a libmp3lame -q:a 0 -f mp3 #{output.path}"
+    when :mp3128k
+      cmd = "ffmpeg -y -nostats -loglevel 0 -i #{input.path} -codec:a libmp3lame -b:a 128k -f mp3 #{output.path}"
+    else
+      raise ArgumentError, "unsupported format: #{format}"
+    end
+
     system(cmd)
   end
 end
