651798f9809978aa45658a57ff7e6062bea349b0
Chris Lowis <chris.lowis@gmail.com>
2023-10-17 20:54:12 +0100

    Add ZipDownloadJob for creating downloadable albums
    This job creates a zip file containing all of the transcoded files of
    the given format.
    
    I've taken a fairly brute force approach to this at the moment - we
    first download all of the transcodes from the asset storage (i.e. S3
    in production) to a temporary directory, then create a zip file from
    these files using rubyzip. We then upload that zip file back to S3
    attached to a new Download record. There's possibly a less memory
    intensive way of doing this and I think there's a slight risk with
    storing files in a temporary directory if machines are restarted
    etc. But hopefully this will be robust enough to get us going.
    
    We only need one download asset per transcode format, so we delete any
    existing ones on creation of a new one.

---
+++ test/jobs/zip_download_job_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'zip'
+
+class ZipDownloadJobTest < ActiveJob::TestCase
+  test 'creates a zip file' do
+    album = create(:album)
+    track1 = create(:track, album:, title: 'First Track', position: 1)
+    track2 = create(:track, album:, title: 'Second Track', position: 2)
+    create(:transcode, track: track1, format: :mp3v0)
+    create(:transcode, track: track2, format: :mp3v0)
+
+    ZipDownloadJob.perform_now(album)
+
+    assert_equal 1, album.downloads.count
+
+    entries = []
+    album.downloads.last.file.open do |zip_file|
+      Zip::File.open(zip_file) do |zip|
+        zip.each do |entry|
+          entries << entry.name
+        end
+      end
+    end
+
+    assert entries.include? '01 - First Track.mp3'
+    assert entries.include? '02 - Second Track.mp3'
+  end
+
+  test 'replaces existing zip file' do
+    album = create(:album)
+    track = create(:track, album:)
+    create(:transcode, track:, format: :mp3v0)
+    create(:download, album:, format: :mp3v0)
+
+    assert_equal 1, album.downloads.count
+    ZipDownloadJob.perform_now(album)
+    assert_equal 1, album.reload.downloads.count
+  end
+end
