5121bba1879473ea19555a7721760ab04fba18ee
Chris Lowis <chris.lowis@gmail.com>
2023-10-23 15:48:03 +0100

    Add a zip file to download factory
    So that we can check for its presence in tests. I created this zip
    file using
    
        zip album.zip track.mp3
    
    in the test/fixtures/files/ directory.

--- test/factories/downloads.rb
+++ test/factories/downloads.rb
@@ -2,7 +2,15 @@
 
 FactoryBot.define do
   factory :download do
-    format { 1 }
+    format { :mp3v0 }
     album { nil }
+
+    after(:build) do |transcode|
+      transcode.file.attach(
+        io: Rails.root.join('test/fixtures/files/album.zip').open,
+        filename: 'album.zip',
+        content_type: 'application/zip'
+      )
+    end
   end
 end
