9dffd64e8579e4a2b5cc0e12c804124d00c905f3
James Mead <james@floehopper.org>
2023-11-04 16:42:58 +0000

    Extract assert_contains_pair in TranscodeCommandTest

--- test/models/transcode_command_test.rb
+++ test/models/transcode_command_test.rb
@@ -25,50 +25,50 @@ class TranscodeCommandTest < ActiveSupport::TestCase
 
   test 'only show fatal errors which could lead the process to crash, such as an assertion failure' do
     command_string = TranscodeCommand.new(@input, @output, :mp3v0).generate
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-loglevel', '0'] })
+    assert_contains_pair(command_string, ['-loglevel', '0'])
   end
 
   test 'specifies input file' do
     command_string = TranscodeCommand.new(@input, @output, :mp3v0).generate
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-i', @input.path] })
+    assert_contains_pair(command_string, ['-i', @input.path])
   end
 
   test 'adds image to output' do
     image = Tempfile.create('transcode-image')
     command_string = TranscodeCommand.new(@input, @output, :mp3v0, {}, image).generate
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-i', image.path] })
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-map', '0:0'] })
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-map', '1:0'] })
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-c', 'copy'] })
+    assert_contains_pair(command_string, ['-i', image.path])
+    assert_contains_pair(command_string, ['-map', '0:0'])
+    assert_contains_pair(command_string, ['-map', '1:0'])
+    assert_contains_pair(command_string, ['-c', 'copy'])
   end
 
   test 'adds metadata using ID3v2.3 format' do
     metadata = { track_title: 'track-title', album_title: 'album-title', artist_name: 'artist-name' }
     command_string = TranscodeCommand.new(@input, @output, :mp3v0, metadata).generate
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-write_id3v2', '1'] })
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-id3v2_version', '3'] })
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-metadata', 'TIT2="track-title"'] })
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-metadata', 'TALB="album-title"'] })
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-metadata', 'TPE1="artist-name"'] })
+    assert_contains_pair(command_string, ['-write_id3v2', '1'])
+    assert_contains_pair(command_string, ['-id3v2_version', '3'])
+    assert_contains_pair(command_string, ['-metadata', 'TIT2="track-title"'])
+    assert_contains_pair(command_string, ['-metadata', 'TALB="album-title"'])
+    assert_contains_pair(command_string, ['-metadata', 'TPE1="artist-name"'])
   end
 
   test 'transcodes audio to mp3 using libmp3lame codec highest audio quality' do
     command_string = TranscodeCommand.new(@input, @output, :mp3v0).generate
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-codec:a', 'libmp3lame'] })
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-q:a', '0'] })
+    assert_contains_pair(command_string, ['-codec:a', 'libmp3lame'])
+    assert_contains_pair(command_string, ['-q:a', '0'])
   end
 
   test 'transcodes audio to mp3128k using libmp3lame codec with bitrate of 128k & file format mp3' do
     command_string = TranscodeCommand.new(@input, @output, :mp3128k).generate
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-codec:a', 'libmp3lame'] })
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-b:a', '128k'] })
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-f', 'mp3'] })
+    assert_contains_pair(command_string, ['-codec:a', 'libmp3lame'])
+    assert_contains_pair(command_string, ['-b:a', '128k'])
+    assert_contains_pair(command_string, ['-f', 'mp3'])
   end
 
   test 'transcodes audio to flac using flac codec with file format flac' do
     command_string = TranscodeCommand.new(@input, @output, :flac).generate
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-codec:a', 'flac'] })
-    assert(command_string.split.each_cons(2).any? { |pair| pair == ['-f', 'flac'] })
+    assert_contains_pair(command_string, ['-codec:a', 'flac'])
+    assert_contains_pair(command_string, ['-f', 'flac'])
   end
 
   test 'specifies output file' do
@@ -86,5 +86,11 @@ class TranscodeCommandTest < ActiveSupport::TestCase
     command_string = command.generate
     command.expects(:system).with(command_string, exception: true)
     command.execute
+  end
+
+  private
+
+  def assert_contains_pair(string, expected_pair)
+    assert(string.split.each_cons(2).any? { |pair| pair == expected_pair })
   end
 end
