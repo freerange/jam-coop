486124eb9c5677dbe4964360c2b06d89679d6e06
Chris Lowis <chris.lowis@gmail.com>
2023-11-04 21:26:00 +0000

    Add system test coverage for creating an album
    This test adds some basic coverage of the happy path of adding and
    album and its tracks through the new combined edit page.

---
+++ test/system/creating_an_album_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class CreatingAnAlbumTest < ApplicationSystemTestCase
+  setup do
+    @artist = create(:artist)
+
+    sign_in_as(create(:user))
+  end
+
+  test 'creating an album' do
+    visit artist_url(@artist)
+    click_link 'Add album'
+    fill_in 'Title', with: "A Hard Day's Night"
+    attach_file 'Cover', Rails.root.join('test/fixtures/files/cover.png')
+
+    within('#tracks') do
+      click_link 'Add track'
+      fill_in 'Title', with: 'And I Love Her'
+      attach_file 'File', Rails.root.join('test/fixtures/files/track.wav')
+    end
+
+    click_button 'Save'
+
+    assert_text "A Hard Day's Night (unpublished)"
+    click_link "A Hard Day's Night (unpublished)"
+    assert_text '1. And I Love Her'
+  end
+end
