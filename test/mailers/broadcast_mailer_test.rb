9558e1ad3cb4cb210cf9bfc9b7d4569954f4981c
Chris Lowis <chris.lowis@gmail.com>
2023-11-09 20:50:00 +0000

    Fix BroadcastMailerTest
    I broke these in the previous commit.

--- test/mailers/broadcast_mailer_test.rb
+++ test/mailers/broadcast_mailer_test.rb
@@ -3,23 +3,23 @@
 require 'test_helper'
 
 class BroadcastMailerTest < ActionMailer::TestCase
-  test 'build test email' do
+  test 'build newsletter email' do
     user = build(:user, email: 'ann@example.com')
 
-    mail = BroadcastMailer.test(user)
+    mail = BroadcastMailer.newsletter(user)
 
-    assert_equal 'Testing BroadcastMailer', mail.subject
+    assert_equal 'jam.coop - Newsletter #1', mail.subject
     assert_equal ['ann@example.com'], mail.to
     assert_equal ['contact@jam.coop'], mail.from
-    assert_match 'Testing, testing, 123 - is this thing on?', mail.body.encoded
+    assert_match 'Hello', mail.body.encoded
     assert_equal 'true', mail.message.track_opens
     assert_equal 'broadcast', mail.message.message_stream
   end
 
-  test 'do not send test email if user has sending suppressed' do
+  test 'do not send newsletter email if user has sending suppressed' do
     user = create(:user, sending_suppressed_at: Time.current)
 
-    BroadcastMailer.test(user).deliver_now!
+    BroadcastMailer.newsletter(user).deliver_now!
 
     assert_emails 0
   end
