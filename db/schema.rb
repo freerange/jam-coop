4f1893d0f57c29cfc5f1a45ff0ecfaed0abd7071
Chris Lowis <chris.lowis@gmail.com>
2023-11-13 15:40:06 +0000

    Add admin boolean to User
    We need a way of distinguishing between admin users (who can currently
    do anything) and other users. I considered adding a "role" here, but I
    think we can differentiate between admins and users who can administer
    artists by the `has_many :artists` association.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_13_134528) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_13_153733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -143,6 +143,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_13_134528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "sending_suppressed_at"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
