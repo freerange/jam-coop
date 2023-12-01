82f11c1f8c6cff9020def96dc965f7f1a914630e
James Mead <james@floehopper.org>
2023-12-01 15:45:34 +0000

    Add prod & dev stacks in hard-coded account & region

--- aws-cdk/bin/music-coop.ts
+++ aws-cdk/bin/music-coop.ts
@@ -3,19 +3,15 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { MusicCoopStack } from '../lib/music-coop-stack';
 
+const AWS_ACCOUNT_ID = '746135554472'
+const AWS_REGION = 'us-west-2'
+
 const app = new cdk.App();
-new MusicCoopStack(app, 'MusicCoopStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new MusicCoopStack(app, 'MusicCoopProductionStack', {
+  env: { account: AWS_ACCOUNT_ID, region: AWS_REGION },
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new MusicCoopStack(app, 'MusicCoopDevelopmentStack', {
+  env: { account: AWS_ACCOUNT_ID, region: AWS_REGION },
 });
