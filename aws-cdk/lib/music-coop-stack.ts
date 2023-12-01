77f16cc2a3a989ed7ec95aeb6c5c9421f2b7193f
James Mead <james@floehopper.org>
2023-12-01 15:45:34 +0000

    Add prod & dev stacks in hard-coded account & region

--- aws-cdk/lib/music-coop-stack.ts
+++ aws-cdk/lib/music-coop-stack.ts
@@ -1,16 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MusicCoopStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MusicCoopQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
