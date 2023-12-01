6221349fa219188d553d0735b99995837b72d30c
Chris Lowis <chris.lowis@gmail.com>
2023-12-01 14:49:46 +0000

    Add a system test for creating artists

---
+++ test/system/creating_an_artist_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class CreatingAnArtistTest < ApplicationSystemTestCase
+  setup do
+    user = create(:user)
+
+    log_in_as(user)
+  end
+
+  test 'creating an artist' do
+    visit root_url
+    click_button 'avatar'
+    click_link 'Add artist'
+    fill_in 'Name', with: 'The Beatles'
+    fill_in 'Location', with: 'Liverpool'
+    fill_in 'Description', with: 'A popular beat combo'
+    attach_file 'Profile picture', Rails.root.join('test/fixtures/files/cover.png')
+
+    click_button 'Save'
+
+    assert_text 'The Beatles'
+    assert_text 'Liverpool'
+    assert_text 'A popular beat combo'
+  end
+end
