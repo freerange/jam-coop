8475a14b0fe3d534a0ea9ad280e8625b7f523afb
James Mead <james@floehopper.org>
2023-12-01 13:09:37 +0000

    Add skeleton AWS CDK app for managing infrastructure
    I ran the following command in a `<project-root>/music-coop` directory:
    
        cdk init app --language typescript
    
    I then renamed `<project-root>/music-coop` -> `<project-root>/aws-cdk`.
    I did this renaming dance, because the `cdk init` command uses the
    current directory name to name a bunch of things in the new AWS CDK
    project.

---
+++ aws-cdk/test/music-coop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MusicCoop from '../lib/music-coop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/music-coop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MusicCoop.MusicCoopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
