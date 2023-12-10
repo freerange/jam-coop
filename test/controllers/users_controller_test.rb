6fe8451e07ca390df2929cd5b106aec3a447ee9d
Chris Lowis <chris.lowis@gmail.com>
2023-12-10 09:09:47 +0000

    Add a "My account" page
    We want a page to group various actions that a user might want to
    take on their own account (changing passwords, setting up artist
    payout details etc) as these are currently listed flat under the
    avatar menu.
    
    I've decided to route this singular route `/account` to the
    `users#show` action as that seems most appropriate.

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UsersControllerTestSignedIn < ActionDispatch::IntegrationTest
+  setup do
+    @user = create(:user)
+    log_in_as(@user)
+  end
+
+  test '#show' do
+    get account_url
+    assert_response :success
+  end
+end
+
+class UserControllerTestSignedOut < ActionDispatch::IntegrationTest
+  test '#show' do
+    get account_url
+    assert_redirected_to log_in_path
+  end
+end
