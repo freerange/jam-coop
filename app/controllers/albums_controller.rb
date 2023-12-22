69032340989138daf86a156a7dc5298c750cdcf0
James Mead <james@floehopper.org>
2023-12-22 22:03:25 +0000

    Extract AlbumsController#authorize_album for before_action

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,22 +4,13 @@ class AlbumsController < ApplicationController
   before_action :build_album, only: %i[new create]
   before_action :set_album, except: %i[new create]
   skip_before_action :authenticate, only: %i[show]
+  before_action :authorize_album
 
-  def show
-    authorize @album
-  end
-
-  def new
-    authorize @album
-  end
-
-  def edit
-    authorize @album
-  end
+  def show; end
+  def new; end
+  def edit; end
 
   def create
-    authorize @album
-
     if @album.save
       redirect_to artist_url(artist), notice: 'Artist was successfully created.'
     else
@@ -28,8 +19,6 @@ class AlbumsController < ApplicationController
   end
 
   def update
-    authorize @album
-
     if @album.update(album_params)
       redirect_to artist_album_url(@album.artist, @album), notice: 'Artist was successfully updated.'
     else
@@ -38,23 +27,17 @@ class AlbumsController < ApplicationController
   end
 
   def publish
-    authorize @album
-
     @album.publish
     AlbumMailer.with(album: @album).published.deliver_later
     redirect_to artist_album_url(@album.artist, @album)
   end
 
   def unpublish
-    authorize @album
-
     @album.unpublish
     redirect_to artist_album_url(@album.artist, @album)
   end
 
   def request_publication
-    authorize @album
-
     @album.pending
     AlbumMailer.with(album: @album).request_publication.deliver_later
     redirect_to artist_album_url(@album.artist, @album),
@@ -79,5 +62,9 @@ class AlbumsController < ApplicationController
     params
       .require(:album)
       .permit(:title, :price, :cover, :about, :credits, :released_at, tracks_attributes: %i[id title original _destroy])
+  end
+
+  def authorize_album
+    authorize @album
   end
 end
