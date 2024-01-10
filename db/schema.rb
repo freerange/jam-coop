5a64b3de23e02db7877571df8d18b3ff4de2955c
James Mead <james@floehopper.org>
2024-01-10 22:11:40 +0000

    Handle subscription changes for purchase email addresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_07_113949) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_10_214125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -118,6 +118,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_07_113949) do
     t.string "customer_email"
     t.integer "amount_tax"
     t.bigint "user_id"
+    t.datetime "sending_suppressed_at"
     t.index ["album_id"], name: "index_purchases_on_album_id"
     t.index ["user_id"], name: "index_purchases_on_user_id"
   end
