186edd045b63d755d7b7daf5056eaa6453012f89
Ross Chapman <rosschapman@users.noreply.github.com>
2024-02-19 14:32:16 -0800

    Updates tests

--- test/system/managing_an_album_test.rb
+++ test/system/managing_an_album_test.rb
@@ -2,22 +2,24 @@
 
 require 'application_system_test_case'
 
-class CreatingAnAlbumTest < ApplicationSystemTestCase
+class ManagingAnAlbumTest < ApplicationSystemTestCase
   setup do
     @artist = create(:artist)
     user = create(:user, artists: [@artist])
-    @license = create(:license)
+    create_list(:license, 2)
 
     log_in_as(user)
   end
 
   test 'creating an album' do
+    license_select_first = License.first.label
+    license_select_second = License.second.label
     visit artist_url(@artist)
     click_link 'Add album'
     fill_in 'Title', with: "A Hard Day's Night"
     attach_file 'Cover', Rails.root.join('test/fixtures/files/cover.png')
     fill_in 'Released on', with: '2024/01/30'
-    select @license.label, from: 'album_license_id'
+    select license_select_first, from: 'album_license_id'
 
     within('#tracks') do
       click_link 'Add track'
@@ -30,5 +32,14 @@ class CreatingAnAlbumTest < ApplicationSystemTestCase
     assert_text "A Hard Day's Night (unpublished)"
     click_link "A Hard Day's Night (unpublished)"
     assert_text '1. And I Love Her'
+
+    click_link 'Edit album'
+    fill_in 'Title', with: 'New Title', id: 'album_title'
+    select license_select_second, from: 'album_license_id'
+
+    click_button 'Save'
+
+    assert_text 'New Title'
+    assert_text license_select_second
   end
 end
