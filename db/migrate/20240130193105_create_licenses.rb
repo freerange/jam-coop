5f7329308226c23366d40d473d4aa2ebce3d67bd
Ross Chapman <rosschapman@users.noreply.github.com>
2024-02-19 12:42:53 -0800

    Fixes migrations from merge

--- db/migrate/20240130193105_create_licenses.rb
+++ db/migrate/20240130193105_create_licenses.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateLicenses < ActiveRecord::Migration[7.1]
   def change
     create_table :licenses do |t|
       t.string :code, null: false
       t.text :source, null: true
-      t.text :display_text, null: false
+      t.text :label, null: false
       t.timestamps
     end
   end
