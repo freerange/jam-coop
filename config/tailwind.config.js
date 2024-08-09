959534d006f329d967ceef34ed088589a28dc89a
Chris Lowis <chris.lowis@gmail.com>
2024-08-09 13:24:43 +0100

    Configure tailwind to use components directory

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
+    './app/components/**/*.{erb,haml,html,slim}',
     './app/form_builders/*.rb',
     './lib/renderers/*.rb'
   ],
