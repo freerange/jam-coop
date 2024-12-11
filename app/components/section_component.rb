a5066db9145abab2a7f59edc58117910616fd0ef
Chris Lowis <chris.lowis@gmail.com>
2024-12-11 14:31:22 -0500

    Make link optional in SectionComponent

--- app/components/section_component.rb
+++ app/components/section_component.rb
@@ -3,7 +3,7 @@
 class SectionComponent < ViewComponent::Base
   include ApplicationHelper
 
-  def initialize(title:, link_text:, link_path:)
+  def initialize(title:, link_text: nil, link_path: nil)
     @title = title
     @link_text = link_text
     @link_path = link_path
