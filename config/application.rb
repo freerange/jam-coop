4a331b413a012c0b6acdc2648b06e45ad2fb25ab
Chris Lowis <chris.lowis@gmail.com>
2024-12-19 14:32:29 -0500

    Set cache_format_version = 7.1
    We're not using the cache so far, so I think it's fine to use the new default.
    
    Setting it in application.rb as suggested by new_framework_defaults_7_1.rb

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module MusicCoop
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.active_support.cache_format_version = 7.1
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
