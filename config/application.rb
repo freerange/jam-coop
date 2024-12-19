2cca1fdc80f1cc64b2bc136c845a1f551ec78229
Chris Lowis <chris.lowis@gmail.com>
2024-12-19 14:50:01 -0500

    Adopt 7.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module MusicCoop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     config.active_support.cache_format_version = 7.1
 
