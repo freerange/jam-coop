6ca63b77d4eb76a168adbdda7f271c3ed6d6ebda
Chris Lowis <chris.lowis@gmail.com>
2024-12-20 14:43:14 -0500

    Remove the toast 1s after adding it
    We also make sure we remove the entire div and not just fade the toast
    itself, this ensures that the overlay doesn't interfere with other UI
    elements.

--- app/javascript/controllers/toasts_controller.js
+++ app/javascript/controllers/toasts_controller.js
@@ -3,7 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["toast"]
 
+  connect() {
+    setTimeout(() => {
+      this.toastTarget.remove()
+    }, 1000)
+  }
+
   close() {
-    this.toastTarget.classList.add("opacity-0")
+    this.toastTarget.remove();
   }
 }
