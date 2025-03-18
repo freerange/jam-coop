431a827d0f81932df634d1b0bd3180989d28e53f
Seb Jacobs <me@sebjacobs.com>
2025-03-18 17:11:28 +0000

    Allow users to link to a Stripe account
    The Stripe Connect user flow is kicked off by the user from their account page.
    
    * Click "Connect Stripe" button on account page.
    * The `StripeConnectAccountsController#create` action creates a
      `Stripe::Account` for the user and its identifier is persisted in a
      `StripeConnectAccount` record. Note that the `Stripe::Account` isn't
      actually a Stripe account; it's just a reference to a Stripe account.
    * Then there is a redirect to the `link` action where we generate a
      `Stripe::AccountLink` which acts as a single-use token we can use to
      redirect the user over to Stripe to fill in the required information to
      setup their account. Note that they can either use an existing Stripe
      account or create a new one as part of this workflow.
    * If the `Stripe::AccountLink` expires the user is redirected back to
      the `link` action again where a new `Stripe::ActionLink` is created
      and the process starts again.
    * If the user completes the workflow successfully, they are redirected
      back to the `success` action where we update the status of their
      account in the `StripeConnectAccount` record.
    * The user can then see the status of their account on their account
      page.

--- app/models/stripe_connect_account.rb
+++ app/models/stripe_connect_account.rb
@@ -4,4 +4,14 @@ class StripeConnectAccount < ApplicationRecord
   belongs_to :user
 
   validates :stripe_identifier, presence: true
+
+  def status
+    if charges_enabled?
+      'charges_enabled'
+    elsif details_submitted?
+      'details_submitted'
+    else
+      'not_started'
+    end
+  end
 end
