b8cb3db2d6c6629ead8af01ee32e39502d5af9a7
Chris Lowis <chris.lowis@gmail.com>
2025-05-02 15:10:40 +0100

    Run bin/rails solid_queue:install

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter = :resque
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
   # config.active_job.queue_name_prefix = "music_coop_production"
 
   # Disable caching for Action Mailer templates even if Action Controller
