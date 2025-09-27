fc7a9613a4bd046a5a1bd64e938acacd95d8b440
Chris Lowis <chris.lowis@gmail.com>
2025-09-27 11:00:25 +0100

    Add tests for button component

---
+++ test/components/button_component_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ButtonComponentTest < ViewComponent::TestCase
+  def setup
+    render_inline(
+      ButtonComponent.new(
+        text: 'Button',
+        path: 'https://example.com'
+      )
+    ).to_html
+  end
+
+  def test_component_renders_title
+    assert_text('Button')
+  end
+
+  def test_component_renders_a_form_that_posts
+    assert_selector('form[method="post"]')
+  end
+
+  def test_component_renders_a_form_with_path_as_action
+    assert_selector('form[action="https://example.com"]')
+  end
+end
