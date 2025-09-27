6424c9a8956a19c39354293edb84c60efe5ec870
Chris Lowis <chris.lowis@gmail.com>
2025-09-27 11:16:45 +0100

    Remove w-full from default ButtonComponent classes
    This makes it easier to have buttons that are not full width.

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -9,7 +9,7 @@ class ButtonComponent < ViewComponent::Base
   end
 
   def classes
-    default_classes = %w[w-full py-3 bg-amber-500 hover:bg-amber-400 text-white font-medium cursor-pointer
+    default_classes = %w[p-3 bg-amber-500 hover:bg-amber-400 text-white font-medium cursor-pointer
                          disabled:bg-slate-300 disabled:cursor-not-allowed]
     default_classes << @additional_classes[:class]
     default_classes.flatten.join(' ')
