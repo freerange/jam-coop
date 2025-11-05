991a15ab4ab4279898ce10173a69566e7f618ff5
Chris Roos <chris@seagul.co.uk>
2025-11-05 16:55:00 +0000

    Configure bullet to run in development and test
    By running `rails g bullet:install`.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,15 @@ require 'active_support/core_ext/integer/time'
 Rails.application.routes.default_url_options = { host: 'localhost', port: 3000 }
 
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.alert         = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+  end
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,6 +10,12 @@ require 'active_support/core_ext/integer/time'
 Rails.application.routes.default_url_options[:host] = 'example.com'
 
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.bullet_logger = true
+    Bullet.raise         = true # raise an error if n+1 query occurs
+  end
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
