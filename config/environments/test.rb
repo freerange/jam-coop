f8b9197aae99f7997a26d5b6ae5a20df5c72635a
Chris Roos <chris@seagul.co.uk>
2025-11-10 12:34:27 +0000

    Enable Prosopite in test and development

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -68,4 +68,8 @@ Rails.application.configure do
   config.active_job.queue_adapter = :test
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.after_initialize do
+    Prosopite.raise = true
+  end
 end
