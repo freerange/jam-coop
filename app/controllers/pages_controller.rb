3fe1af58ef918c9e548ea421748010620f232b27
Chris Roos <chris@seagul.co.uk>
2025-11-11 11:32:13 +0000

    Load recently released albums in `PagesController#home`
    In preparation for including associated data to avoid N+1 queries.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate
   before_action :skip_authorization
 
-  def home; end
+  def home
+    @recently_released_albums = Album.published.recently_released.limit(4)
+  end
+
   def about; end
   def terms; end
   def blog; end
