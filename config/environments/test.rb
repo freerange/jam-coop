ee4823d20d77c947928b53fdb660bc77f238dc5a
Chris Roos <chris@seagul.co.uk>
2025-11-12 11:08:23 +0000

    Remove duplicate default_url_options from test.rb
    This is also set to `{ host: 'example.com' }` on line 45 and that
    appears to take priority:
    
    ```
    $ rails r -e test "p Rails.application.config.action_mailer.default_url_options"
    {host: "example.com"}
    ```

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,7 +10,6 @@ require 'active_support/core_ext/integer/time'
 Rails.application.routes.default_url_options[:host] = 'example.com'
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # While tests run files are not watched, reloading is not necessary.
