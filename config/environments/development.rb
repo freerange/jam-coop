0f9173d3826cc5be2a4a85841dd583c3b3057057
Chris Roos <chris@seagul.co.uk>
2025-11-12 10:34:44 +0000

    Remove duplicate lint from development.rb
    These default_url_options are also set on line 58.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,6 @@ require 'active_support/core_ext/integer/time'
 Rails.application.routes.default_url_options = { host: 'localhost', port: 3000 }
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
