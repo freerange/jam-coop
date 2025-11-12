7375c58d377c995819ad2d79831ab935647b1861
James Mead <james@floehopper.org>
2025-11-12 15:02:08 +0000

    Add beginnings of player page for OpenGraph/oEmbed
    I've added this action in a separate controller rather than adding an
    action to the `AlbumsController`, because it feels like a different
    concern and the access requirements are different.
    
    I've nested the new route under the artists & albums resources so that
    it's easy to construct from an an album URL and we can use similar
    parameter parsing / model lookup as in the `AlbumsController`.
    
    I'm hoping that this approach means it'll be relatively easy to support
    using the `PlayerController` for a single track if we decide to do that.

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class PlayersController < ApplicationController
+  skip_before_action :authenticate
+
+  helper_method :album, :artist
+
+  def show
+    skip_authorization
+  end
+
+  private
+
+  def album
+    @album ||= artist.albums.friendly.find(params[:album_id])
+  end
+
+  def artist
+    @artist ||= Artist.friendly.find(params[:artist_id])
+  end
+end
