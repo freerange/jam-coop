45694ffb00e0dac848ae9345c6166d1364afb8ce
James Mead <james@floehopper.org>
2025-11-12 12:48:43 +0000

    Avoid some hanging indents
    Somewhat subjective, but I find these versions easier to read.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -40,8 +40,12 @@ class AlbumsController < ApplicationController
   end
 
   def set_album
-    @album = artist.albums.includes(tracks: [{ transcodes: { file_attachment: :blob } },
-                                             { original_attachment: :blob }]).friendly.find(params[:id])
+    @album = artist.albums.includes(
+      tracks: [
+        { transcodes: { file_attachment: :blob } },
+        { original_attachment: :blob }
+      ]
+    ).friendly.find(params[:id])
   end
 
   def artist
