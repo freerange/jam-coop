70a1d3e431f1ec83ebc470b9c9c150f997fcd12a
James Mead <james@floehopper.org>
2025-11-13 16:20:45 +0000

    Combine cdn config with URL options config
    These seems sufficiently closely related that it makes sense to combine
    them into a single file.

--- config/initializers/cdn.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-cdn_base_uri = URI.parse(Rails.configuration.cdn_base_url)
-
-Rails.configuration.cdn_url_options = {
-  protocol: cdn_base_uri.scheme,
-  host: cdn_base_uri.host,
-  port: cdn_base_uri.port
-}
--- config/initializers/url_options.rb
+++ config/initializers/url_options.rb
@@ -7,3 +7,11 @@ Rails.application.default_url_options = {
   host: base_uri.host,
   port: base_uri.port
 }
+
+cdn_base_uri = URI.parse(Rails.configuration.cdn_base_url)
+
+Rails.configuration.cdn_url_options = {
+  protocol: cdn_base_uri.scheme,
+  host: cdn_base_uri.host,
+  port: cdn_base_uri.port
+}
