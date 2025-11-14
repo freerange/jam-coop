1d8b558565ca06e6c7c7caa3f490e0370db688ee
Chris Roos <chris@seagul.co.uk>
2025-11-14 15:50:07 +0000

    Remove manifest.js
    This isn't required by Propshaft.

--- app/assets/config/manifest.js
+++
@@ -1,6 +0,0 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/assets/javascript .js
-//= link_tree ../builds
-//= link tom-select.css
