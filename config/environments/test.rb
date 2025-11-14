0b4d0d34f953a294bb91aefda24c975e25bc92e0
Chris Roos <chris@seagul.co.uk>
2025-11-14 12:08:55 +0000

    Update config/environments/test.rb
    By running `rails app:update`, deleting all the comments, running
    `rubocop -A config/environments/test.rb` and accepting the relevant
    changes.
    
    `perform_caching` was removed from the test.rb.tt template file in PR
    52999[1] which suggests that caching is fine in test. The default value
    is nil and the Rails guide suggests that this means it's set to true by
    default[2][3].
    
    ```
    $ rails r -e test "p Rails.configuration.action_controller.perform_caching"
    nil
    $ rails r -e test "p Rails.configuration.action_mailer.perform_caching"
    nil
    ```
    
    `active_job.queue_adapter` appears to be set to `:test` by default:
    
    ```
    $ rails r -e test "p Rails.configuration.active_job.queue_adapter"
    :test
    ```
    
    Both `disallowed_deprecation` and `disallowed_deprecation_warnings` were
    removed in Rails PR 51831[4]. We haven't overridden them so it seems
    fine for us to remove them too.
    
    ```
    $ rails r -e test "p Rails.configuration.active_support.disallowed_deprecation"
    nil
    $ rails r -e test "p Rails.configuration.active_support.disallowed_deprecation_warnings"
    nil
    ```
    
    [1]: https://github.com/rails/rails/pull/52999/commits/ff98d0886b73e72b5f9a4f25b1b2ae36af86eb89
    [2]: https://guides.rubyonrails.org/configuring.html#config-action-controller-perform-caching
    [3]: https://guides.rubyonrails.org/configuring.html#config-action-mailer-perform-caching
    [4]: https://github.com/rails/rails/pull/51831/commits/506d728412b1c77838e3a7257964a6c549fc3311

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,10 +6,9 @@ Rails.application.configure do
   config.enable_reloading = false
   config.eager_load = ENV['CI'].present?
 
-  config.public_file_server.headers = { 'Cache-Control' => "public, max-age=#{1.hour.to_i}" }
+  config.public_file_server.headers = { 'cache-control' => 'public, max-age=3600' }
 
   config.consider_all_requests_local = true
-  config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
   config.action_dispatch.show_exceptions = :rescuable
@@ -18,14 +17,9 @@ Rails.application.configure do
 
   config.active_storage.service = :test
 
-  config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :test
 
   config.active_support.deprecation = :stderr
-  config.active_support.disallowed_deprecation = :raise
-  config.active_support.disallowed_deprecation_warnings = []
-
-  config.active_job.queue_adapter = :test
 
   config.action_controller.raise_on_missing_callback_actions = true
 
