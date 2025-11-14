ecf8fbb3b9ee7e9980e3be8df35419f076d3c439
Chris Roos <chris@seagul.co.uk>
2025-11-14 15:46:35 +0000

    Move tom-select assets to vendor/assets
    Propshaft automatically includes the `vendor/assets` directory in its
    search path[1] so this should make it easier to switch to Propshaft.
    
    [1]: https://github.com/rails/propshaft/blob/main/UPGRADING.md#asset-paths

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+//= link_tree ../../../vendor/assets/javascript .js
 //= link_tree ../builds
 //= link tom-select.css
