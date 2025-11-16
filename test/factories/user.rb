182a4fea962557fd5ba4300f9c1953f6ac82455d
James Mead <james@floehopper.org>
2025-11-16 15:53:35 +0000

    Hard-code user password digest in factory
    Since generating the password digest is quite a computationally
    expensive operation, we might as well pre-compute the value.

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "person#{n}@example.com" }
-    password_digest { BCrypt::Password.create('Secret1*3*5*') }
+    password_digest { '$2a$12$J5ftEJ8yxoPj6oLuenHNQOPPghdQWwVeB4Puj5o7d8W6uaTdCQyGS' }
     verified { true }
   end
 end
