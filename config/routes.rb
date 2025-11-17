6e440082ed902b3ed6160f7de9eaff77ab3bf257
Chris Lowis <chris.lowis@gmail.com>
2025-11-17 16:14:19 +0000

    Remove admin/albums route
    This route was obsoleted by 2e32724 so can be removed.

--- config/routes.rb
+++ config/routes.rb
@@ -40,8 +40,7 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resources 'albums', only: %i[index]
-    resources 'newsletters', only: %i[new create edit update]
+    resources 'newsletters', only: %i[index new create edit update]
   end
 
   resources :purchases, only: %i[show]
