c7cdcbbb48f5a46e12a20b7765227bd15de00179
James Mead <james@floehopper.org>
2025-11-20 16:16:26 +0000

    Refactor StripeWebhookEventsControllerTest
    I want to make it easier to test that the controller can handle another
    type of event. The new approach builds just enough of a Stripe event to
    satisfy the controller and generates a real Stripe signature for the
    request.
    
    Note that I've had to set the Stripe `endpoint_secret` to a non-empty
    value in order to avoid an error in the test.

--- test/controllers/stripe_webhook_events_controller_test.rb
+++ test/controllers/stripe_webhook_events_controller_test.rb
@@ -4,50 +4,65 @@ require 'test_helper'
 
 class StripeWebhookEventsControllerTest < ActionDispatch::IntegrationTest
   test 'checkout.session.completed marks associated purchase as complete' do
-    PurchaseCompleteJob.expects(:perform_later).with(stripe_session_id, customer_email, amount_tax)
-    Stripe::Webhook.stubs(:construct_event).with(payload, 'signature', nil).returns(stripe_event)
+    checkout_session_id = 'cs_test_a11boUfs0kYSNA6S3K3z9XdMuXj5RwIdguh67hwQL47YmOjoa1WcUeWivv'
+    customer_email = 'customer@example.com'
+    amount_tax = 140
 
-    post stripe_webhook_events_path, env: { 'RAW_POST_DATA' => payload, 'HTTP_STRIPE_SIGNATURE' => 'signature' }
+    params = checkout_session_completed_params(checkout_session_id, customer_email, amount_tax)
+    headers = headers_for_event(params)
+
+    PurchaseCompleteJob.expects(:perform_later).with(checkout_session_id, customer_email, amount_tax)
+
+    post stripe_webhook_events_path, params:, headers:, as: :json
 
     assert_response :success
   end
 
   test 'invalid json in payload' do
-    Stripe::Webhook.stubs(:construct_event).with('invalid-json', 'signature', nil).raises(JSON::ParserError)
+    Stripe::Webhook.stubs(:construct_event).raises(JSON::ParserError)
 
-    post stripe_webhook_events_path, env: { 'RAW_POST_DATA' => 'invalid-json', 'HTTP_STRIPE_SIGNATURE' => 'signature' }
+    post stripe_webhook_events_path
 
     assert_response :bad_request
   end
 
-  test 'an event that is not checkout.session.completed' do
-    event = stub(type: 'an.other.event')
-    Stripe::Webhook.stubs(:construct_event).with(payload, 'signature', nil).returns(event)
+  test 'an unrecognized event' do
+    params = { type: 'an.other.event' }
+    headers = headers_for_event(params)
 
-    post stripe_webhook_events_path, env: { 'RAW_POST_DATA' => payload, 'HTTP_STRIPE_SIGNATURE' => 'signature' }
+    post stripe_webhook_events_path, params:, headers:, as: :json
 
     assert_response :created
   end
 
   private
 
-  def payload
-    Rails.root.join('test/fixtures/files/stripe/checkout.session.completed.json').read
+  def checkout_session_completed_params(checkout_session_id, customer_email, amount_tax)
+    {
+      type: 'checkout.session.completed',
+      data: {
+        object: {
+          id: checkout_session_id,
+          customer_details: {
+            email: customer_email
+          },
+          total_details: {
+            amount_tax:
+          }
+        }
+      }
+    }
   end
 
-  def stripe_session_id
-    JSON.parse(payload)['data']['object']['id']
+  def headers_for_event(params)
+    payload = params.to_json
+    timestamp = Time.current
+    signature = Stripe::Webhook::Signature.compute_signature(timestamp, payload, webhook_secret)
+    signature_header = Stripe::Webhook::Signature.generate_header(timestamp, signature)
+    { 'Stripe-Signature' => signature_header }
   end
 
-  def customer_email
-    JSON.parse(payload)['data']['object']['customer_details']['email']
-  end
-
-  def amount_tax
-    JSON.parse(payload)['data']['object']['total_details']['amount_tax']
-  end
-
-  def stripe_event
-    Stripe::Event.construct_from(JSON.parse(payload), symbolize_names: true)
+  def webhook_secret
+    StripeWebhookEventsController::ENDPOINT_SECRET
   end
 end
