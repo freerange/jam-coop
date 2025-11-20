bfb507177851d87455a2366a698d1d3c8b8e94b8
Chris Lowis <chris.lowis@gmail.com>
2025-11-20 15:18:43 +0000

    Label has_one_attached logo

--- test/factories/label.rb
+++ test/factories/label.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
   factory :label do
     user
     name { 'Jam Records' }
+
+    after(:build) do |label|
+      label.logo.attach(
+        io: Rails.root.join('test/fixtures/files/cover.png').open,
+        filename: 'cover.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end
