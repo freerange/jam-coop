ffdf42a5f0e051b7a5e01fd0fbe15a886e960be6
Chris Lowis <chris.lowis@gmail.com>
2025-11-21 11:29:24 +0000

    Add Newsletter#send_as_email
    This method is designed to be run from the rails console in the first
    instance and will send a newsletter to the correct recipients.
    
    We want to send the newsletter to all Users with verified email
    addresses, who have not opted out of receiving the newsletter and
    whose email addresses are not supressed for other reasons (e.g. bounce
    etc).
    
    We also want to send the newsletter to the people who previously
    registered interest, confirmed their email address and who are not
    suppressed.
    
    We don't have to worry about duplicate emails between the two lists at
    the moment because I ensured that anyone on the Interest list who is
    also a User was removed from the Interest list by running:
    
        signed_up = []
        Interest.find_each {|i| signed_up << i if User.exists?(email: i.email)}
        signed_up.each(&:destroy)
    
    in production.

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -3,4 +3,16 @@
 class Newsletter < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  def send_as_email
+    users = User.where(verified: true, opt_in_to_newsletter: true, sending_suppressed_at: nil)
+    users.find_each do |user|
+      BroadcastMailer.with(recipient: user, newsletter: self).newsletter.deliver_later
+    end
+
+    interests = Interest.where(email_confirmed: true, sending_suppressed_at: nil)
+    interests.find_each do |interest|
+      BroadcastMailer.with(recipient: interest, newsletter: self).newsletter.deliver_later
+    end
+  end
 end
--- test/models/newsletter_test.rb
+++ test/models/newsletter_test.rb
@@ -3,7 +3,72 @@
 require 'test_helper'
 
 class NewsletterTest < ActiveSupport::TestCase
+  include ActionMailer::TestHelper
+
   test 'factory is valid' do
     assert build(:newsletter).valid?
+  end
+
+  test '#send delivers to Users who have opted in, verified and are not suppressed' do
+    newsletter = create(:newsletter)
+    create(:user, verified: true, opt_in_to_newsletter: true, sending_suppressed_at: nil)
+
+    assert_enqueued_emails 1 do
+      newsletter.send_as_email
+    end
+  end
+
+  test '#send does not deliver to Users who have opted out' do
+    newsletter = create(:newsletter)
+    create(:user, verified: true, opt_in_to_newsletter: false, sending_suppressed_at: nil)
+
+    assert_no_enqueued_emails do
+      newsletter.send_as_email
+    end
+  end
+
+  test '#send does not deliver to Users who are suppressed' do
+    newsletter = create(:newsletter)
+    create(:user, verified: true, opt_in_to_newsletter: true, sending_suppressed_at: Time.zone.now)
+
+    assert_no_enqueued_emails do
+      newsletter.send_as_email
+    end
+  end
+
+  test '#send does not deliver to Users who are not verified' do
+    newsletter = create(:newsletter)
+    create(:user, verified: false, opt_in_to_newsletter: true, sending_suppressed_at: nil)
+
+    assert_no_enqueued_emails do
+      newsletter.send_as_email
+    end
+  end
+
+  test '#send delivers to Interests who have confirmed and are not suppressed' do
+    newsletter = create(:newsletter)
+    create(:interest, email_confirmed: true, sending_suppressed_at: nil)
+
+    assert_enqueued_emails 1 do
+      newsletter.send_as_email
+    end
+  end
+
+  test '#send does not deliver to Interests who are not confirmed' do
+    newsletter = create(:newsletter)
+    create(:interest, email_confirmed: false, sending_suppressed_at: nil)
+
+    assert_no_enqueued_emails do
+      newsletter.send_as_email
+    end
+  end
+
+  test '#send does not deliver to Interests who are suppressed' do
+    newsletter = create(:newsletter)
+    create(:interest, email_confirmed: true, sending_suppressed_at: Time.zone.now)
+
+    assert_no_enqueued_emails do
+      newsletter.send_as_email
+    end
   end
 end
