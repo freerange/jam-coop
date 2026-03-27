df6bc2b0b9dc69de25a721c81c3102bb0702c944
Chris Lowis <chris.lowis@gofreerange.com>
2026-03-27 12:13:20 +0000

    Allow ButtonComponent to generate links
    Passing `link: true` causes ButtonComponent to generate a link tag
    styled to look like a button.

--- test/components/button_component_test.rb
+++ test/components/button_component_test.rb
@@ -40,4 +40,19 @@ class ButtonComponentTest < ViewComponent::TestCase
   def test_component_adds_classes_to_default
     assert_selector('button.additional-css-class')
   end
+
+  def test_component_can_render_a_link
+    render_inline(
+      ButtonComponent.new(
+        text: 'Link',
+        path: 'https://example.com',
+        link: true,
+        class: ['additional-css-class']
+      )
+    )
+
+    assert_selector('a[href="https://example.com"]')
+    assert_selector('a[text()="Link"]')
+    assert_selector('a.additional-css-class')
+  end
 end
