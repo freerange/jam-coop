22c4f67a6e15d1ceef3bb9cec62f55b5c04f254e
Chris Lowis <chris.lowis@gofreerange.com>
2026-03-27 15:44:33 +0000

    Allow new track to be added from album edit page
    This commit introduces new and create Track actions with corresponding
    views. This is on the way to refactoring the album creation UI to use
    more idiomatic rails concepts.

--- test/controllers/admin/tracks_controller_test.rb
+++ test/controllers/admin/tracks_controller_test.rb
@@ -3,10 +3,50 @@
 require 'test_helper'
 
 module Admin
-  class TracksControllerTest < ActionDispatch::IntegrationTest
+  class TracksControllerTestAsArtist < ActionDispatch::IntegrationTest
     setup do
-      log_in_as(create(:user, admin: true))
-      @track = create(:track, album: build(:draft_album))
+      @album = create(:draft_album)
+      user = create(:user)
+      user.artists << @album.artist
+      log_in_as(user)
+    end
+
+    test 'should get new' do
+      get new_admin_artist_album_track_path(@album.artist, @album)
+      assert_response :success
+    end
+
+    test 'should create track' do
+      assert_difference('Track.count') do
+        post admin_artist_album_tracks_path(@album.artist, @album), params: {
+          track: { title: 'New Track', original: fixture_file_upload('one.wav', 'audio/wav') }
+        }
+      end
+      assert_redirected_to edit_admin_artist_album_path(@album.artist, @album)
+    end
+  end
+
+  class TracksControllerTestAsAdmin < ActionDispatch::IntegrationTest
+    setup do
+      @admin = create(:user, admin: true)
+      @album = create(:draft_album)
+      @track = create(:track, album: @album)
+
+      log_in_as(@admin)
+    end
+
+    test 'should get new' do
+      get new_admin_artist_album_track_path(@album.artist, @album)
+      assert_response :success
+    end
+
+    test 'should create track' do
+      assert_difference('Track.count') do
+        post admin_artist_album_tracks_path(@album.artist, @album), params: {
+          track: { title: 'New Track', original: fixture_file_upload('one.wav', 'audio/wav') }
+        }
+      end
+      assert_redirected_to edit_admin_artist_album_path(@album.artist, @album)
     end
 
     test 'should move track higher' do
