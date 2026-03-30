5d677d3195e1bf4aed5d039cbcf025d54a67a610
James Mead <james@floehopper.org>
2026-03-30 16:27:06 +0100

    Exclude purchases w/ payouts from purchase export
    Purchases with payouts will be purchases where a payment has been made to the
    seller via a Stripe destination charge and so we don't want to accidentally pay
    them again via the out-of-band payout process via Wise.

--- test/lib/purchase_exporter_test.rb
+++ test/lib/purchase_exporter_test.rb
@@ -25,6 +25,16 @@ class PurchaseExporterTest < ActiveSupport::TestCase
     assert_not_includes export, excluded_purchase.id
   end
 
+  test 'export only includes purchases without a corresponding payout' do
+    included_purchase = create(:purchase, completed: true, created_at: Date.new(2023, 12, 1))
+    payout = build(:stripe_payout)
+    excluded_purchase = create(:purchase, completed: true, created_at: Date.new(2023, 12, 1), payout:)
+    export = PurchaseExporter.new(@today).to_csv
+
+    assert_includes export, included_purchase.id
+    assert_not_includes export, excluded_purchase.id
+  end
+
   test 'export includes purchase details' do
     create(:purchase, price: 7.00, amount_tax: 123, created_at: Date.new(2023, 12, 1))
 
