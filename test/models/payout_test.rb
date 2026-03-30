cba9f11d32ef1dad617f8b6ea4e30b3eabaae59b
James Mead <james@floehopper.org>
2026-03-30 15:24:41 +0100

    Add Payout model
    This is intended to represent a _generic_ payout such that it _could_ be used
    to represent the payouts that are currently made out-of-band via Wise.
    
    However, my actual motivation for adding the model is to capture a payout to a
    seller with a connected Stripe account.
    
    I struggled a bit with the naming, because in Stripe terminology the payment
    from the Jam Stripe account to the seller Stripe account is called a "transfer"
    and it's only the payment from the seller Stripe account into their bank
    account that's called a "payout". However, given the existence of the
    `PayoutDetails` model and in the context of the Jam app, I think `Payout` does
    make sense.

---
+++ test/models/payout_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class PayoutTest < ActiveSupport::TestCase
+  test 'factory is valid' do
+    assert build(:payout).valid?
+  end
+
+  test 'payout_type is required' do
+    assert_not build(:payout, payout_type: nil).valid?
+  end
+
+  test 'transaction_reference is required' do
+    assert_not build(:payout, transaction_reference: nil).valid?
+  end
+
+  test 'destination_reference is required' do
+    assert_not build(:payout, destination_reference: nil).valid?
+  end
+
+  test 'amount_in_pence is required' do
+    assert_not build(:payout, amount_in_pence: nil).valid?
+  end
+
+  test 'platform_fee_in_pence is required' do
+    assert_not build(:payout, platform_fee_in_pence: nil).valid?
+  end
+
+  test 'belongs to user' do
+    user = build(:user)
+    payout = create(:payout, user:)
+    assert_equal user, payout.user
+  end
+end
