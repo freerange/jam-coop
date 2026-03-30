763ae26f732c720e0edc39acfe599a8ba957c5ae
James Mead <james@floehopper.org>
2026-03-30 16:23:37 +0100

    Include payout details in the notify_artist email
    I think it makes more sense to base the logic on whether a Stripe-related
    payout exists rather than whether a Stripe account is connected.
    
    It's helpful to the seller to see the payout amount and the platform fee that's
    been deducted.
    
    This diff is best viewed using the `--ignore-all-space` option, because a bunch
    of the changes are just changes to indentation.

--- test/mailers/purchase_mailer_test.rb
+++ test/mailers/purchase_mailer_test.rb
@@ -34,20 +34,23 @@ class PurchaseMailerTest < ActionMailer::TestCase
     assert_includes mail.body.to_s, 'for £7.00'
   end
 
-  test 'notify_artist with connected Stripe account accepting payments' do
+  test 'notify_artist with Stripe payout associated with purchase' do
     stripe_connect_account = build(:stripe_connect_account, charges_enabled: true)
     user = build(:user, stripe_connect_account:)
     album = build(:album)
     user.artists << album.artist
-    purchase = build(:purchase, album:, price: 7.00)
+    payout = build(:stripe_payout, amount_in_pence: 550, platform_fee_in_pence: 150)
+    purchase = build(:purchase, album:, price: 7.00, payout:)
 
     mail = PurchaseMailer.with(purchase:).notify_artist
 
     assert_equal "You have sold a copy of #{album.title}", mail.subject
     assert_equal [user.email], mail.to
     assert_includes mail.body.to_s, 'for £7.00'
-    assert_includes mail.body.to_s, 'A payment has been made to your'
+    assert_includes mail.body.to_s, 'A payment of £5.50 has been made to your'
     assert_includes mail.body.to_s, link_stripe_connect_account_url(stripe_connect_account.stripe_identifier)
+    assert_includes mail.body.to_s, 'A platform fee of £1.50 was deducted.'
+    assert_includes mail.body.to_s, 'Other Stripe fees may have been incurred (e.g. currency conversion fees).'
   end
 
   test 'notify_artist with connected Stripe account not accepting payments' do
