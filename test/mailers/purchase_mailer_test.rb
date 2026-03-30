dd0534ee2ccdbd6e3a0342e4af935305c030e04c
James Mead <james@floehopper.org>
2026-03-30 21:32:31 +0100

    Use seller vs user to be more intention-revealing

--- test/mailers/purchase_mailer_test.rb
+++ test/mailers/purchase_mailer_test.rb
@@ -22,30 +22,30 @@ class PurchaseMailerTest < ActionMailer::TestCase
   end
 
   test 'notify_artist' do
-    user = build(:user)
+    seller = build(:user)
     album = build(:album)
-    user.artists << album.artist
+    seller.artists << album.artist
     purchase = create(:purchase, album:, price: 7.00)
 
     mail = PurchaseMailer.with(purchase:).notify_artist
 
     assert_equal "You have sold a copy of #{album.title}", mail.subject
-    assert_equal [user.email], mail.to
+    assert_equal [seller.email], mail.to
     assert_includes mail.body.to_s, 'for £7.00'
   end
 
   test 'notify_artist with Stripe payout associated with purchase' do
     stripe_connect_account = build(:stripe_connect_account, charges_enabled: true)
-    user = build(:user, stripe_connect_account:)
+    seller = build(:user, stripe_connect_account:)
     album = build(:album)
-    user.artists << album.artist
+    seller.artists << album.artist
     payout = build(:stripe_payout, amount_in_pence: 550, platform_fee_in_pence: 150)
     purchase = create(:purchase, album:, price: 7.00, payout:)
 
     mail = PurchaseMailer.with(purchase:).notify_artist
 
     assert_equal "You have sold a copy of #{album.title}", mail.subject
-    assert_equal [user.email], mail.to
+    assert_equal [seller.email], mail.to
     assert_includes mail.body.to_s, 'for £7.00'
     assert_includes mail.body.to_s, 'A payment of £5.50 has been made to your'
     assert_includes mail.body.to_s, link_stripe_connect_account_url(stripe_connect_account.stripe_identifier)
@@ -55,9 +55,9 @@ class PurchaseMailerTest < ActionMailer::TestCase
 
   test 'notify_artist with connected Stripe account not accepting payments' do
     stripe_connect_account = build(:stripe_connect_account, details_submitted: true)
-    user = build(:user, stripe_connect_account:)
+    seller = build(:user, stripe_connect_account:)
     album = build(:album)
-    user.artists << album.artist
+    seller.artists << album.artist
     purchase = create(:purchase, album:, price: 7.00)
 
     mail = PurchaseMailer.with(purchase:).notify_artist
@@ -77,9 +77,9 @@ class PurchaseMailerTest < ActionMailer::TestCase
   end
 
   test 'notify_artist asks user to complete payout details' do
-    user = build(:user)
+    seller = build(:user)
     album = build(:album)
-    user.artists << album.artist
+    seller.artists << album.artist
     purchase = create(:purchase, album:, price: 7.00)
 
     mail = PurchaseMailer.with(purchase:).notify_artist
@@ -87,10 +87,10 @@ class PurchaseMailerTest < ActionMailer::TestCase
   end
 
   test 'notify_artist allows user to change/verify payout details' do
-    user = build(:user)
-    build(:payout_detail, user:)
+    seller = build(:user)
+    build(:payout_detail, user: seller)
     album = build(:album)
-    user.artists << album.artist
+    seller.artists << album.artist
     purchase = create(:purchase, album:, price: 7.00)
 
     mail = PurchaseMailer.with(purchase:).notify_artist
@@ -98,9 +98,9 @@ class PurchaseMailerTest < ActionMailer::TestCase
   end
 
   test 'do not notify artist of purchase if sending is suppressed' do
-    user = build(:user, sending_suppressed_at: Time.current)
+    seller = build(:user, sending_suppressed_at: Time.current)
     album = build(:album)
-    user.artists << album.artist
+    seller.artists << album.artist
     purchase = create(:purchase, album:, price: 7.00)
 
     PurchaseMailer.with(purchase:).notify_artist.deliver_now!
