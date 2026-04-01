7ff10600da809f5d7652d561c9083b3ae3ab0273
James Mead <james@floehopper.org>
2026-04-01 17:20:15 +0100

    Allow overriding model name in ModelErrorComponent
    I want to be able to capitalize proper nouns for `StripeConnectAccount`, i.e.
    converting it to "Stripe Connect account" and that's not possible with the
    default behaviour.

--- app/components/model_error_component.rb
+++ app/components/model_error_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class ModelErrorComponent < ViewComponent::Base
-  def initialize(model:)
+  def initialize(model:, model_name: nil)
     @model = model
+    @model_name = model_name || model.model_name.human.downcase
   end
 
   def render?
