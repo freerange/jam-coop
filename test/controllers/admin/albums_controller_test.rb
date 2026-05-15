3ff0368a8f2c8c1fbe69d316f556f1f88425b02f
Chris Lowis <chris.lowis@gofreerange.com>
2026-05-15 13:49:06 +0100

    Remove Track editing section from albums/new, albums/edit
    We have a replacement UI for managing an album's tracks now so this
    section can be removed. I've also removed the associated stimulus
    controllers, and included the `upload_controller` which was originally
    used for this purpose but I must have forgotten to remove at some point.

--- test/controllers/admin/albums_controller_test.rb
+++ test/controllers/admin/albums_controller_test.rb
@@ -30,17 +30,6 @@ module Admin
       assert_redirected_to admin_artist_album_path(@album.artist, Album.last)
     end
 
-    test '#create enqueues transcoding' do
-      assert_enqueued_with(job: TranscodeJob) do
-        post admin_artist_albums_path(@album.artist), params: {
-          album: { title: 'Example',
-                   cover: fixture_file_upload('cover.png'),
-                   license_id: License.first.id,
-                   tracks_attributes: { '0': { title: 'Test', original: fixture_file_upload('one.wav') } } }
-        }
-      end
-    end
-
     test '#edit' do
       get edit_admin_artist_album_path(@album.artist, @album)
       assert_response :success
@@ -49,24 +38,6 @@ module Admin
     test '#update' do
       patch admin_artist_album_path(@album.artist, @album), params: { album: { title: 'Example' } }
       assert_redirected_to admin_artist_album_path(@album.artist, @album)
-    end
-
-    test '#update accepts attributes for tracks' do
-      track = create(:track, album: @album, title: 'Old name')
-
-      patch admin_artist_album_path(@album.artist, @album),
-            params: { album: { title: 'Example', tracks_attributes: { '0': { id: track.id, title: 'New name' } } } }
-
-      assert_equal 'New name', track.reload.title
-    end
-
-    test '#update allows tracks to be destroyed' do
-      track = create(:track, album: @album, title: 'Old name')
-
-      assert_difference('Track.count', -1, 'A Track should be destroyed') do
-        patch admin_artist_album_path(@album.artist, @album),
-              params: { album: { title: 'Example', tracks_attributes: { '0': { id: track.id, _destroy: true } } } }
-      end
     end
   end
 
