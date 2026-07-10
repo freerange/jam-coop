8e6af88464d499c75a038a09c9c858703b08bb5f
James Mead <james.mead@gofreerange.com>
2026-07-10 16:23:27 +0100

    Add more previews for PurchaseMailer#notify_artist

--- test/mailers/previews/purchase_mailer_preview.rb
+++ test/mailers/previews/purchase_mailer_preview.rb
@@ -19,4 +19,34 @@ class PurchaseMailerPreview < ActionMailer::Preview
 
     PurchaseMailer.with(purchase:).notify_artist
   end
+
+  # Preview this email at http://localhost:3000/rails/mailers/purchase_mailer/notify_artist_with_stripe_payout
+  def notify_artist_with_stripe_payout
+    seller = build(:user)
+    stripe_payout = build(:stripe_payout)
+    purchase = build_stubbed(:purchase, payout: stripe_payout)
+    purchase.define_singleton_method(:seller) { seller }
+
+    PurchaseMailer.with(purchase:).notify_artist
+  end
+
+  # Preview this email at http://localhost:3000/rails/mailers/purchase_mailer/notify_artist_with_stripe_account_not_accepting_payments
+  def notify_artist_with_stripe_account_not_accepting_payments
+    stripe_connect_account = build(:stripe_connect_account)
+    seller = build(:user, stripe_connect_account:)
+    purchase = build_stubbed(:purchase)
+    purchase.define_singleton_method(:seller) { seller }
+
+    PurchaseMailer.with(purchase:).notify_artist
+  end
+
+  # Preview this email at http://localhost:3000/rails/mailers/purchase_mailer/notify_artist_without_stripe_account_but_with_payout_detail
+  def notify_artist_without_stripe_account_but_with_payout_detail
+    payout_detail = build(:payout_detail)
+    seller = build(:user, payout_detail:)
+    purchase = build_stubbed(:purchase)
+    purchase.define_singleton_method(:seller) { seller }
+
+    PurchaseMailer.with(purchase:).notify_artist
+  end
 end
