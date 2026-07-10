81be950c7985e62c4e6f1fa5f5afc3172ad4311c
James Mead <james.mead@gofreerange.com>
2026-07-10 16:15:51 +0100

    Add preview for PurchaseMailer#completed

--- test/mailers/previews/purchase_mailer_preview.rb
+++ test/mailers/previews/purchase_mailer_preview.rb
@@ -4,6 +4,13 @@
 class PurchaseMailerPreview < ActionMailer::Preview
   include FactoryBot::Syntax::Methods
 
+  # Preview this email at http://localhost:3000/rails/mailers/purchase_mailer/completed
+  def completed
+    purchase = build_stubbed(:purchase, customer_email: 'customer@example.com')
+
+    PurchaseMailer.with(purchase:).completed
+  end
+
   # Preview this email at http://localhost:3000/rails/mailers/purchase_mailer/notify_artist
   def notify_artist
     seller = build(:user)
