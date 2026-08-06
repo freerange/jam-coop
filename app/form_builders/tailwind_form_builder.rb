dbb1784c39d17d64fe75aedffac9a8883a2370e8
James Mead <james.mead@gofreerange.com>
2026-08-06 21:17:19 +0100

    Extract TailwindFormBuilder#image_field
    To reduce duplication and ensure consistency.

--- app/form_builders/tailwind_form_builder.rb
+++ app/form_builders/tailwind_form_builder.rb
@@ -74,6 +74,10 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
     @template.content_tag('div', label + error_label, { class: 'flex flex-col items-start' })
   end
 
+  def image_field(method, options = {})
+    file_field(method, options.with_defaults(accept: Image.content_types))
+  end
+
   private
 
   def text_like_field(field_method, object_method, options = {})
