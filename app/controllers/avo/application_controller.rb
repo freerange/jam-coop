4e090319ef86df68d883cc37c07d8a10bc69455d
Chris Lowis <chris.lowis@gmail.com>
2026-08-19 07:47:02 +0100

    Authenticate /avo routes
    Avo supports more sophisticated authorization using pundit[1] but I
    don't think we need that just yet. This change prevents any non-admin
    user from hitting the avo pages.
    
    [1] https://docs.avohq.io/4.0/authorization.html

--- app/controllers/avo/application_controller.rb
+++ app/controllers/avo/application_controller.rb
@@ -2,5 +2,14 @@
 
 module Avo
   class ApplicationController < BaseApplicationController
+    include RequestContext
+
+    before_action :authenticate
+
+    private
+
+    def authenticate
+      redirect_to main_app.root_path unless ::Current.user.admin?
+    end
   end
 end
